8abf2e4363a44a8a77c62f8359ad26efed040a9a
Wilton Max <102773414+Wilton-Max@users.noreply.github.com>
2024-06-16 02:06:57 -0300

    Commit um

---
+++ README.md
@@ -0,0 +1,14 @@
+
+**ELEMENTOS USADOS**
+
+*_Flexbox:_*
+https://getbootstrap.com/docs/5.3/utilities/flex/
+
+*_container:_*
+https://getbootstrap.com/docs/5.0/layout/containers/
+
+*_Cards:_*
+https://getbootstrap.com/docs/5.3/components/card/
+
+*_Buttons:_*
+https://getbootstrap.com/docs/5.3/components/button-group/
---
+++ script.js
@@ -0,0 +1,18 @@
+
+let colorRed    =   document.getElementById('red')
+let colorYellow =   document.getElementById('yellow')
+let colorGreen  =   document.getElementById('green')
+
+
+    
+function mudarRed() {
+    colorRed.src   = "img/vermelho.png";
+}
+
+function mudarYellow() {
+    colorYellow.src   = "img/mostarda.png";
+}
+
+function mudarGreen() {
+    colorGreen.src   = "img/verde.png";
+}
